aa998d4f98fa966914121687c1a1f55987b9e412
matejmatejmatej <matej.drienovsky@spojenassv.sk>
2026-03-20 22:08:52 +0100

    pecka
    ukladanie mena,ukladanie hry

--- klient - smotný projekt - vytiahni von a importuj/global.gd
+++ klient - smotný projekt - vytiahni von a importuj/global.gd
@@ -10,7 +10,46 @@ var peniaze_level=5
 var max_peniaze_level=1
 var zivoty_level=1
 
+var meno
+
 # ci je potava odomknuta
 var postavy = {
 	"res://nesmej sa/bul/pistol(SILENT STRIKER.png":false,
 	}
+
+var save_path = "user://save.dat"
+
+
+func _ready():
+	load_game()
+
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_game()
+
+
+# ================= SAVE =================
+
+func save_game():
+	var data = {
+		"meno": meno,
+	}
+
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_string(JSON.stringify(data))
+
+
+# ================= LOAD =================
+
+func load_game():
+	if not FileAccess.file_exists(save_path):
+		return
+
+	var file = FileAccess.open(save_path, FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+
+	if data == null:
+		return
+
+	meno= data["meno"]
--- klient - smotný projekt - vytiahni von a importuj/menu/startovascena.gd
+++ klient - smotný projekt - vytiahni von a importuj/menu/startovascena.gd
@@ -1,5 +1,29 @@
 extends Node2D
 
 
+func _ready() -> void:
+
+	if Global.meno==null:
+		$LineEdit.show()
+
+		
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://ClientScene.tscn")
+
+
+func _on_upgrade_pressed() -> void:
+	get_tree().change_scene_to_file("res://ClientScene.tscn")
+
+
+func _on_equip_pressed() -> void:
+	get_tree().change_scene_to_file("res://ClientScene.tscn")
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	Global.meno=$LineEdit.text
+	$LineEdit.hide()
